e47854e3a0f8da7f802e5bb7cf4e2eda191f234d
hoobasurf <brice.paneela@icloud.com>
2025-07-08 21:14:38 +0200

    Update translate.js

--- netlify/functions/translate.js
+++ netlify/functions/translate.js
@@ -1,28 +1,24 @@
-const fetch = require("node-fetch");
+const fetch = require('node-fetch');
 
-exports.handler = async (event) => {
+exports.handler = async function(event, context) {
   try {
     const body = JSON.parse(event.body);
 
-    const response = await fetch("https://translate.argosopentech.com/translate", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
+    const response = await fetch('https://translate.argosopentech.com/translate', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(body)
     });
 
     const data = await response.json();
-
     return {
       statusCode: 200,
       body: JSON.stringify(data)
     };
-
-  } catch (err) {
+  } catch (error) {
     return {
       statusCode: 500,
-      body: JSON.stringify({ error: "Erreur serveur ou API externe" })
+      body: JSON.stringify({ error: 'Erreur API de traduction' })
     };
   }
 };
