4f02271a20a94dd1939ff83abea4c1a6a7b721bd
hoobasurf <brice.paneela@icloud.com>
2025-07-08 14:35:44 +0200

    Create proxy.php

---
+++ public/proxy.php
@@ -0,0 +1,17 @@
+<?php
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+
+$data = json_decode(file_get_contents('php://input'), true);
+
+$options = [
+    'http' => [
+        'header'  => "Content-type: application/json\r\n",
+        'method'  => 'POST',
+        'content' => json_encode($data),
+    ],
+];
+$context  = stream_context_create($options);
+$result = file_get_contents('https://translate.argosopentech.com/translate', false, $context);
+echo $result;
+?>
